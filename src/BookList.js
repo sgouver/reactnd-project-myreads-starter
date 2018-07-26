8317335352c75055c0ec239fb3fe17f4fa43f0bf
Stefanos Gkouveris <stefanos.gkouveris@gmail.com>
2018-07-26 18:24:17 +0100

    setState
    - Search.js component
    - filter per shelf
    - API fetch function
    - Selection 'value' mechanism

---
+++ src/BookList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+
+//Create a component to delete the majority of spagetti code of App.js.
+
+render() {
+
+  return(
+<div className="bookshelf-books">
+ <ol className="books-grid">
+  {this.props.books.map((book) => (
+        <li key={book.id} >
+          <div className="book">
+            <div className="book-top">
+              <div key={book.title} className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                //Set the book image
+                backgroundImage: `Url(${book.imageLinks.thumbnail})`
+                }}>
+              </div>
+                <div className="book-shelf-changer">
+                  <select
+                    onChange = {(event) => this.props.UpdateShelf(
+                      book, event.target.value
+                    )}
+                    >
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+            </div>
+              {/* set book title */}
+            <div className="book-title">{book.title}</div>
+              {/* set book author */}
+            <div className="book-authors">{book.authors}</div>
+          </div>
+          </li>
+        )
+      )}
+    </ol>
+  </div>
+   )
+  }
+}
+
+export default BookList
